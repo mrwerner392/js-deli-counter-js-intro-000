b739bed286143d30cbab6f04b1a22ced5862b9ff
Matt Werner <mrwerner392@gmail.com>
2019-05-14 14:06:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // adds a name to the end of an array and returns a string
 function takeANumber(array, name) {
   array.push(name);
-  return `Welcome, ${name}. You are number ${array.indexOf(name) + 1} in line.`;
+  return `Welcome, ${name}. You are number ${array.length} in line.`;
 }
 
 // returns the first name in an array and removes it from the array, or notifies if array is empty
